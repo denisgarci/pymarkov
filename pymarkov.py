7c9c40167bf486a66ede0f994fd04155d54dd235
Denis Garcia <denisgarci@gmail.com>
2013-12-09 16:48:04 -0500

    Use counter and skip some uppercase word

--- pymarkov.py
+++ pymarkov.py
@@ -1,6 +1,8 @@
+import sys
 from collections import defaultdict
 from random import choice, randrange
-from itertools import izip
+from itertools import izip, islice
+from collections import Counter
 
 from nltk.tokenize import sent_tokenize, word_tokenize
 
@@ -8,21 +10,26 @@ class MarkovChainGenerator(object):
 
     def __init__(self, file_name, k=2):
         self.gram_length = k
-        self.gram_dict = defaultdict(lambda : defaultdict(int))
+        self.gram_dict = defaultdict(Counter)
         self.populate_dict(file_name)
 
     def populate_dict(self, file_name):
+        """ (file_name) -> None
+        Creates and populates self.gram_dict
+        """
         with open(file_name) as word_file:
             text = word_file.read()
             for sent in sent_tokenize(text):
+                if sent.isupper():
+                    continue
                 words = [None] + word_tokenize(sent) + [None]
-                for gram in izip(*[words[i:] for i in range(self.gram_length + 1)]):
-                    key = gram[:self.gram_length]
-                    value = gram[self.gram_length]
-                    self.gram_dict[key][value] += 1
+                for word_tuple in izip(*[words[i:] for i in range(self.gram_length + 1)]):
+                    gram = word_tuple[:self.gram_length]
+                    word = word_tuple[self.gram_length]
+                    self.gram_dict[gram][word] += 1
 
     def generate_sentence(self):
-        """ (self) -> [string]
+        """ () -> [string]
         Generates a sttring using the Markov chain...
         """
         start_keys = [k for k in self.gram_dict.keys() if k[0] is None]
@@ -32,27 +39,21 @@ class MarkovChainGenerator(object):
         while True:
             next_word = self.random_word(self.gram_dict[current_gram])
             if next_word is None:
-                return sentence
+                return ' '.join(sentence)
             sentence.append(next_word)
             current_gram = tuple(sentence[-self.gram_length:])
 
-    def random_word(self, freq_dict):
-        """ (self, dict) -> string
+    def random_word(self, freq_counter):
+        """ (Counter) -> string
         Returns a random word based on the distribution given in the dict
         """
-        total = sum(freq_dict.values())
-        random_index = randrange(total)
-        freq_sum  = 0
-        for k, v in freq_dict.iteritems():
-            freq_sum += v
-            if random_index < freq_sum:
-                return k
-
-
+        random_index = randrange(sum(freq_counter.values()))
+        return next(islice(freq_counter.elements(), random_index, None))
 
 
 if __name__ == '__main__':
-    a = MarkovChainGenerator("moby-dick.txt", 2)
+    a = MarkovChainGenerator("emily-dick.txt", 2)
     #print(a.gram_dict)
-    print(' '.join(a.generate_sentence()))
+    for _ in range(int(sys.argv[1])):
+        print(a.generate_sentence())
 
